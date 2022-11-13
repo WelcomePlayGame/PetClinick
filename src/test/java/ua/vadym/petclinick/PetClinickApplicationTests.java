51b62c148932c078d02273cf9c0f7b6b50aafe2a
Petrovich <welcomeplaygame@gmail.com>
2022-11-13 21:03:49 +0200

    Chance two

---
+++ src/test/java/ua/vadym/petclinick/PetClinickApplicationTests.java
@@ -0,0 +1,13 @@
+package ua.vadym.petclinick;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetClinickApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
